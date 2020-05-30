5a952fa6dfce9385f5f40f3696c7b4780e9337ef
kota_na_ <ad.maiora.1@gmail.com>
2020-05-30 21:01:00 +0900

    feat: サービスインストール feat: 単一exe

--- WindowsServiceWorker/Program.cs
+++ WindowsServiceWorker/Program.cs
@@ -1,28 +1,98 @@
-﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.CommandLineUtils;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace WindowsServiceWorker
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            var isService = !(Debugger.IsAttached || args.ToList().Contains("--console"));
-            var builder = CreateHostBuilder(args);
-            if (isService)
+            var app = new CommandLineApplication();
+            app.Name = "Windows Service Worker";
+            app.Description = "Windows Serviceとして実行します。";
+            app.HelpOption("-h | --help");
+
+            var serviceOption = app.Option(
+                template: "--service",
+                description: "サービスとして実行します",
+                optionType: CommandOptionType.NoValue);
+
+            var consoleOption = app.Option(
+                template: "--console",
+                description: "コンソールとして実行します",
+                optionType: CommandOptionType.NoValue);
+
+            var installOption = app.Option(
+                template: "--install",
+                description: "サービスをインストールします",
+                optionType: CommandOptionType.NoValue);
+
+            var uninstallOption = app.Option(
+                template: "--uninstall",
+                description: "サービスをアンインストールします",
+                optionType: CommandOptionType.NoValue);
+
+            app.OnExecute(() =>
             {
-                await builder.RunAsServiceAsync();
-            }
-            else
-            {
-                await builder.RunConsoleAsync();
-            }
+                var name = "WindowsServiceWorkerTest";
+                var basePath = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+                var path = Path.Combine(basePath, "WindowsServiceWorker.exe");
+
+                var builder = CreateHostBuilder(args);
+                if (serviceOption.HasValue())
+                {
+                    Task.Run(async () => { await builder.RunAsServiceAsync(); }).Wait();
+                    return 0;
+                }
+
+                if (consoleOption.HasValue() || Debugger.IsAttached)
+                {
+                    Task.Run(async () => { await builder.RunConsoleAsync(); }).Wait();
+                    return 0;
+                }
+
+                if (installOption.HasValue())
+                {
+                    ServiceAccessor.Install(name, path);
+                    return 0;
+                }
+
+                if (uninstallOption.HasValue())
+                {
+                    ServiceAccessor.Uninstall(name);
+                    return 0;
+                }
+
+                if (ServiceAccessor.IsInstalled(name))
+                {
+                    var process = new Process();
+                    process.StartInfo.FileName = @"WindowsServiceWorker.exe";
+                    process.StartInfo.Arguments = @"--uninstall";
+                    process.StartInfo.Verb = "RunAs";
+                    process.StartInfo.UseShellExecute = true;
+                    process.Start();
+                    process.WaitForExit();
+                }
+                else
+                {
+                    var process = new Process();
+                    process.StartInfo.FileName = @"WindowsServiceWorker.exe";
+                    process.StartInfo.Arguments = @"--install";
+                    process.StartInfo.Verb = "RunAs";
+                    process.StartInfo.UseShellExecute = true;
+                    process.Start();
+                    process.WaitForExit();
+                }
+
+                return 0;
+            });
+
+            app.Execute(args);
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ WindowsServiceWorker/ServiceAccessor.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+
+namespace WindowsServiceWorker
+{
+    public static class ServiceAccessor
+    {
+        public static bool IsInstalled(string name)
+        {
+            var sc = new ServiceController(name);
+            try
+            {
+                return sc.ServiceName == name;
+            }
+            catch (InvalidOperationException)
+            {
+                return false;
+            }
+        }
+
+        public static void Install(string name, string path)
+        {
+            InstallService(name, path);
+            FailurService(name, "restart", 1000, "restart", 1000, "restart", 1000);
+            StartService(name);
+        }
+
+        public static void Uninstall(string name)
+        {
+            StopService(name);
+            UninstallService(name);
+        }
+
+        private static void InstallService(string name, string path)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = @"sc.exe";
+            proc.StartInfo.Arguments = $"Create {name} binPath=\"{path} --service\" start=auto";
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private static void StartService(string name)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = @"sc.exe";
+            proc.StartInfo.Arguments = $"Start {name}";
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private static void StopService(string name)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = @"sc.exe";
+            proc.StartInfo.Arguments = $"Stop {name}";
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private static void UninstallService(string name)
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = @"sc.exe";
+            proc.StartInfo.Arguments = $"Delete {name}";
+            proc.Start();
+            proc.WaitForExit();
+        }
+
+        private static void FailurService(string name, string act1, int time1, string act2, int time2, string act3, int time3)
+        {
+            var actions = $"{act1}/{time1}/{act2}/{time2}/{act3}/{time3}";
+            var reset = $"{3600}";
+            var proc = new Process();
+            proc.StartInfo.FileName = @"sc.exe";
+            proc.StartInfo.Arguments = $"Failure {name} actions= {actions} reset= {reset}";
+            proc.Start();
+            proc.WaitForExit();
+        }
+    }
+}
